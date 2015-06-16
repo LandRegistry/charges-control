04e43629db83d05b1291c956d4327fc121725d92
Michael Allen <michael@michaelallen.io>
2015-06-16 12:13:02 +0100

    moved the apply commands from the jenkins jobs into a versioned script

---
+++ bin/jenkins.sh
@@ -0,0 +1,17 @@
+sudo /usr/local/bin/librarian-puppet update
+export exitcode=$?
+if [ $exitcode -eq 4 -o $exitcode -eq 6 ] 
+  then exit 1
+fi
+
+sudo /usr/local/bin/librarian-puppet install
+export exitcode=$?
+if [ $exitcode -eq 4 -o $exitcode -eq 6 ] 
+  then exit 1
+fi
+
+sudo FACTER_workspace=$WORKSPACE /bin/puppet apply \
+  --verbose \
+  --debug \
+  --hiera_config=$WORKSPACE/hiera.integration.yaml \
+  --modulepath=$WORKSPACE/site:$WORKSPACE/modules manifests/site.pp
